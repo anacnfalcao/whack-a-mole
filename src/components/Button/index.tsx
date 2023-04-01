2ddc1ee46609861da3502b93acb61fb76195406b
Ana Falcao <afalcao@tillster.com>
2023-04-01 22:15:14 +0100

    feat(button): add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+import styles from './style.module.css'
+import Text from '@/components/Text'
+
+type Props = {
+  label: string;
+  onClick: (params: any) => any;
+  className?: string;
+  style?: React.CSSProperties;
+  textClassName?: string;
+  textStyle?: React.CSSProperties;
+};
+
+export default function Button({className, style, label, onClick, textClassName, textStyle} : Props) : JSX.Element{
+  return (
+    <button  onClick={onClick}  className={[styles.button,, className].join('')} style={style}>
+      <Text label={label} className={textClassName} style={textStyle}/>
+    </button>
+  )
+}
