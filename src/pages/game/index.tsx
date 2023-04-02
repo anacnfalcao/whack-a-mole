7ecd76325fa7b8b2ba246ae663c4806a26b85c31
Ana Falcao <afalcao@tillster.com>
2023-04-02 01:10:43 +0100

    feat(redux): integrate redux with next following tutorial

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -5,14 +5,16 @@ import Text from '@/components/Text'
 import Button from '@/components/Button'
 import { useRouter } from 'next/router'
 import Mole from '@/components/Mole'
-
+import { useDispatch, useSelector } from "react-redux";
+import { selectAuthState, setAuthState }from '@/state/game';
 
 const ROWS = 3;
 const COLUMNS = 4;
 
 
-
 export default function Game() {
+	const authState = useSelector(selectAuthState);
+	const dispatch = useDispatch();
   const router = useRouter()
   const endGame = () => {router.push('/scoreboard')};
   
@@ -50,7 +52,15 @@ export default function Game() {
       <main className={styles.main}>
         <Background>
           <>
+            <Text label={authState + '?'}></Text>
             {board()}
+            <button
+                className={styles.button}
+                aria-label="Increment value"
+                onClick={() => dispatch(setAuthState(true))}
+                >
+                +
+            </button>
           </>
         </Background>
       </main>
